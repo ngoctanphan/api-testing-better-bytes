ce07b67c6e766f7e3365513b6fea78bbe82491f0
unknown <ngoctanphan01@gmail.com>
2025-03-12 23:01:22 +0700

    Exercise 3

--- Lesson_02/ex2.js
+++ Lesson_02/ex2.js
@@ -1,1 +1,7 @@
-const name = "default"
+const myName = "Ryan"
+const myAddress = "Ho Chi Minh City"
+const myFavorites = "Programing"
+const isLearningAPI = true
+console.log("My name: " + myName)
+console.log("My address: " + myAddress)
+console.log("isLearningAPI: " + isLearningAPI)
---
+++ Lesson_02/ex3.js
@@ -0,0 +1,6 @@
+const height = 170
+const idealWeight = ((height -100)*9)/10
+const maxWeight = height - 100
+const minWeight = ((height - 100)*8)/10
+
+console.log("Mức cân nặng lý tưởng, tối thiểu, tối đa lần lượt là: "+ idealWeight, minWeight, maxWeight)
